7175980e7b0057ed3e2fbadf5ab1994296ad6ecc
agions <1051736049@qq.com>
2020-09-03 17:48:15 +0800

    项目init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<div class="about">
+  <h1>This is an about page</h1>
+  <div v-for="item in list" :key="item.id">
+    <div>
+      <div>{{ item.title }}</div>
+      <div>{{ item.description }}</div>
+    </div>
   </div>
+</div>
 </template>
+
+<script>
+/* eslint-disable space-before-function-paren */
+export default {
+  name: "about",
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    this.$axios.get("/api/tasks").then((response) => {
+      const {
+        status,
+        data
+      } = response;
+      if (status === 200) {
+        this.list = data.tasks;
+      }
+    });
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
